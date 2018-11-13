2bf52151fef09a53c937afb90efa51d99b6d681f
Nathan <nathan@jones.one>
2018-11-12 20:50:04 -0500

    Cleaned up code and added dark mode toggle.

--- app/app.js
+++ app/app.js
@@ -3,10 +3,7 @@
 // Declare app level module which depends on views, and core components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version',
-     'ui.bootstrap'
+  'myApp.view1'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
